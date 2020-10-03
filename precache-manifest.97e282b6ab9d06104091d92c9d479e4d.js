384cbd1997e7b32a0e3ee0263ca9cd3af74c21f5
Lev Vasilyev <torikowashi@gmail.com>
2020-10-03 08:09:40 +0300

    Updates

---
+++ precache-manifest.97e282b6ab9d06104091d92c9d479e4d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf99cfe00bd0ae0dd1d893cef4ac279",
+    "url": "/boom-fest/index.html"
+  },
+  {
+    "revision": "3d6add0c794f5a8ba11f",
+    "url": "/boom-fest/static/css/main.5977a3b1.chunk.css"
+  },
+  {
+    "revision": "dd100e10add3f0643a6a",
+    "url": "/boom-fest/static/js/2.27d3307b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boom-fest/static/js/2.27d3307b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6add0c794f5a8ba11f",
+    "url": "/boom-fest/static/js/main.a856816e.chunk.js"
+  },
+  {
+    "revision": "0d65a5d73e6add64d6f1",
+    "url": "/boom-fest/static/js/runtime-main.59399724.js"
+  },
+  {
+    "revision": "eda2eb492f313f06a2874efd578e748f",
+    "url": "/boom-fest/static/media/audio.eda2eb49.mp3"
+  },
+  {
+    "revision": "57da0150efd5b42c45b0505a89c57c20",
+    "url": "/boom-fest/static/media/battery.25.57da0150.svg"
+  },
+  {
+    "revision": "fafa66d723a289433428a907f9a2314e",
+    "url": "/boom-fest/static/media/folder.fafa66d7.svg"
+  },
+  {
+    "revision": "56a7b555f942c98ad5d4b421c18330ae",
+    "url": "/boom-fest/static/media/folder.fill.56a7b555.svg"
+  },
+  {
+    "revision": "b6542af92eedca2936af647012b9e959",
+    "url": "/boom-fest/static/media/greenfrog.b6542af9.png"
+  },
+  {
+    "revision": "e6e8ffef320d375363b56c498f8f0c3b",
+    "url": "/boom-fest/static/media/magnifyingglass.e6e8ffef.svg"
+  },
+  {
+    "revision": "358068ad7b9aa9425b9144c543e746f1",
+    "url": "/boom-fest/static/media/model.358068ad.glb"
+  },
+  {
+    "revision": "dff260f896fe23adb83341639fdf93be",
+    "url": "/boom-fest/static/media/pointer.dff260f8.svg"
+  },
+  {
+    "revision": "16fcbc431dea79a3099fd7efcffcc09c",
+    "url": "/boom-fest/static/media/pointinghand.16fcbc43.svg"
+  },
+  {
+    "revision": "faf86dba7ada7be66aea76047609f93c",
+    "url": "/boom-fest/static/media/wifi.faf86dba.svg"
+  }
+]);
